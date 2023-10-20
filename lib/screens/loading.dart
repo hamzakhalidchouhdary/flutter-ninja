6d119ad18e2d7d968683949ec47d65709a3bd98c
Hamza Khalid Chouhdary <hamzah.khalid@goicon.com>
2023-10-20 15:03:44 +0000

    get ninja data and navigate to home

--- lib/screens/loading.dart
+++ lib/screens/loading.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/services/get_ninja_list.dart';
 
 class LoadingScreen extends StatefulWidget {
   const LoadingScreen({super.key});
@@ -8,11 +9,25 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+
+  NinjaList ninjas = NinjaList();
+
+  void navigateToHome() {
+    Navigator.pushReplacementNamed(context, '/home', arguments: ninjas);
+  }
+
+  void loadNinjsList() async {
+    await ninjas.getNinjaList();
+    navigateToHome();
+  }
+
   @override
   void initState() {
     super.initState();
-    
+    loadNinjsList();
   }
+
+  @override
   Widget build(BuildContext context) {
     return const Scaffold(
       body: Center(
