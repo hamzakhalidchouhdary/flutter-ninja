66a02a7898e70c0b79a54291738c942a914894d6
Hamza Khalid Chouhdary <hamzah.khalid@goicon.com>
2023-12-27 16:53:02 +0500

    enhanced updated screen

--- lib/screens/updates.dart
+++ lib/screens/updates.dart
@@ -5,6 +5,66 @@ class Updates extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text('Updates'));
+    return const Column(
+      children: [
+        Padding(
+          padding: EdgeInsets.all(15.0),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                'Status',
+                style: TextStyle(fontWeight: FontWeight.w600, fontSize: 18.0),
+              ),
+              Icon(Icons.more_vert_rounded),
+            ],
+          ),
+        ),
+        Status()
+      ],
+    );
+  }
+}
+
+class Status extends StatelessWidget {
+  const Status({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    const double avatarRadius = 32.0;
+    return Padding(
+      padding: const EdgeInsets.only(left: 15.0),
+      child: SizedBox(
+        height: 100.0,
+        child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: 50,
+            itemBuilder: (BuildContext context, int index) {
+              return const SizedBox(
+                width: (avatarRadius * 2.0) + 20.0,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    CircleAvatar(
+                      backgroundColor: Colors.amber,
+                      radius: avatarRadius,
+                    ),
+                    Text(
+                      'User First Name',
+                      style: TextStyle(
+                        overflow: TextOverflow.ellipsis,
+                        fontSize: 12.0,
+                      ),
+                    )
+                  ],
+                ),
+              );
+            }),
+      ),
+    );
   }
 }
